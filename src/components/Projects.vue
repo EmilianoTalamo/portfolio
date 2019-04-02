bb47a11cd1757cabd907fb82d03f0fb11c87661d
Emiliano Tálamo <emilianotalamo@gmail.com>
2019-04-02 17:26:31 -0300

    Fixed route paths

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,7 +4,7 @@
 			<li v-for="item in projects" :key="item.folder">
 				<!-- router-link renders into an <a> tag that changes the parameter accordingly to the clicked project, each item has a custom color and an isBright class that makes the text more legible on brighter background. All is set on the data JSON. -->
 				<router-link
-					:to="{ path: 'portfolio/' + item.folder}"
+					:to="{ path: item.folder}"
 					:style=" { backgroundColor: item.color }"
 					:class="{lightItem :item.isbright}"
 				>{{ item.title }}</router-link>
